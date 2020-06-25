affaf6f3588a33144d37ff972482732d92ed351e
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-06-25 20:12:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,11 +43,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    if season == "winter"
-      puts holiday
-    end
-  end
+
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
